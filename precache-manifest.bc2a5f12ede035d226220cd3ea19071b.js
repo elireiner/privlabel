cf2b3da4094afd68ec6dc533d72fa64d33dfc19e
Eli Reiners <goelyukim@gmail.com>
2020-03-02 12:40:44 -0500

    Updates

---
+++ precache-manifest.bc2a5f12ede035d226220cd3ea19071b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5670a4ae3863289e3eba203e63e5903",
+    "url": "/privlabel/index.html"
+  },
+  {
+    "revision": "ee140ad233a9821adf35",
+    "url": "/privlabel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "074d22c998fb6cbde3db",
+    "url": "/privlabel/static/js/2.17c67e35.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/privlabel/static/js/2.17c67e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee140ad233a9821adf35",
+    "url": "/privlabel/static/js/main.3807adfd.chunk.js"
+  },
+  {
+    "revision": "8f5e0404531a05855421",
+    "url": "/privlabel/static/js/runtime-main.6fca6dd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/privlabel/static/media/logo.5d5d9eef.svg"
+  }
+]);
