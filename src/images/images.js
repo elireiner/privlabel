0896d926fe90a76228c3185be7eaf833a7b61947
Eli Reiners <goelyukim@gmail.com>
2020-03-19 15:34:28 -0400

    Add images

---
+++ src/images/images.js
@@ -0,0 +1,28 @@
+const images = [
+    {
+        id: 1,
+        src: "https://images-na.ssl-images-amazon.com/images/I/61aho5PeFzL._AC_SL1169_.jpg"
+    },
+    {
+        id: 2,
+        src: "https://images-na.ssl-images-amazon.com/images/I/81KwzNTjQ7L._AC_SL1500_.jpg"
+    },
+    {
+        id: 3,
+        src: "https://images-na.ssl-images-amazon.com/images/I/91uYMkMjdxL._AC_SL1500_.jpg"
+    },
+    {
+        id: 4,
+        src: "https://images-na.ssl-images-amazon.com/images/I/91FfAF6dx8L._AC_SL1500_.jpg"
+    },
+    {
+        id: 5,
+        src: "https://images-na.ssl-images-amazon.com/images/I/91QfH1epM5L._AC_SL1500_.jpg"
+    },
+    {
+        id: 6,
+        src: "https://images-na.ssl-images-amazon.com/images/I/A1kHwpNK-dL._AC_SL1500_.jpg"
+    }
+]
+
+export default images;
