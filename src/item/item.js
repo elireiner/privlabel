f89d19a8f36e233877fe93bb03a59a3962115daf
Eli Reiners <goelyukim@gmail.com>
2020-03-23 11:41:43 -0400

    Use state to change image on timer

--- src/item/item.js
+++ src/item/item.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './item.css';
 import images from "../images/images"
-class Item extends Component{
+
+let img = images.map(({ id, src }) => <img src={src} key={id} />)
+class Item extends Component {
+  state = {
+    img: 0
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        img: this.state.img + 1
+      })
+    }, 3000)
+  }
+
   render() {
-    return(
-        <div className="item-image">
-            {images.map(({id, src}) => <img //src={src}
-             key={id} />)}
-        </div>
-        
+    return (
+      <div className="item-image">
+        {img[this.state.img]}
+      </div>
+
     )
   }
 }
