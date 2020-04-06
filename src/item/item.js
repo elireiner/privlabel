4604e4d6827a5bbb30d0aa2f88334e0a345d3bcc
Eli Reiners <goelyukim@gmail.com>
2020-04-06 11:36:55 -0400

    Add alt and key to img and fix issue with index

--- src/item/item.js
+++ src/item/item.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import './item.css';
 import images from "../images/images"
 
-let imgArray = images.map(({src, id}) => { return {src, id}})
+let imgArray = images.map(({ src, id }) => { return { src, id } })
+console.log(imgArray)
 class Item extends Component {
   state = {
     img: 0
   }
- 
+
   componentDidMount() {
     this.intervalID = setInterval(() => {
-      if (this.state.img < imgArray.length){
+      if (this.state.img < imgArray.length -1) {
         this.setState({
-        img: this.state.img + 1
-      })
-    } else {
-      this.setState({
-        img: 0
-      })
-    }}, 2000)
+          img: this.state.img + 1
+        })
+      } else {
+        this.setState({
+          img: 0
+        })
+      }
+    }, 2000)
   }
 
   componentWillUnmount() {
@@ -28,7 +30,12 @@ class Item extends Component {
   render() {
     return (
       <div className="item-images">
-        <img className="item-image" src={imgArray[this.state.img].src} />
+        <img
+          className="item-image"
+          src={imgArray[this.state.img].src}
+          key={imgArray[this.state.img].id}
+          alt="image of the product"
+        />
       </div>
 
     )
